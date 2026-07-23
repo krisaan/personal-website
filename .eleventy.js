89247d774a74e45a92689bec6ab4668fbfb433a2
krisaan <knokuri@gmail.com>
2026-07-23 12:56:55 -0400

    Add path prefix

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,10 @@
 module.exports = function (eleventyConfig) {
-    // Tell Eleventy to copy the assets folder straight to the _site directory
+    eleventyConfig.addPassthroughCopy("src/style.css");
+    eleventyConfig.addPassthroughCopy("src/script.js");
     eleventyConfig.addPassthroughCopy("src/assets");
 
-    // Your other configurations (e.g., passthrough for script.js, style.css, etc.)
-    eleventyConfig.addPassthroughCopy("src/script.js");
-    eleventyConfig.addPassthroughCopy("src/style.css");
-
     return {
+        pathPrefix: "/personal-website/", // Replace with your exact GitHub repository name
         dir: {
             input: "src",
             output: "_site"
